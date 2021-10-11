484ffc0be1f956fdd9f101c3e6795bbad5c2b41f
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-11 14:09:29 +0530

    update

--- LineComp.java
+++ LineComp.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class LineComp {
 	public static void main(String[] args){
 		  Scanner sc =new Scanner(System.in);
-		  System.out.println("enter 4 no");
+		  System.out.println("enter x co-ordinate 1st points");
 		   int x1=sc.nextInt();
+		System.out.println("enter x co-ordinate 2nd points");
 		   int x2=sc.nextInt();
+		System.out.println("enter y co-ordinate 1st points");
 		   int y1=sc.nextInt();
+		System.out.println("enter y co-ordinate 2nd points");
 		   int y2=sc.nextInt();
 		 double length=Math.sqrt((Math.pow(x2-x1,2))+Math.pow(y2-y1,2));
 		   System.out.println("length="+length);
-		  System.out.println("enter next line 4 no");
+		 System.out.println("enter x co-ordinate 1st points");
 		    int x3=sc.nextInt();
+		System.out.println("enter x co-ordinate 2nd points");
 		   int x4=sc.nextInt();
+		System.out.println("enter y co-ordinate 1st points");
 		   int y3=sc.nextInt();
+		System.out.println("enter y co-ordinate 2nd points");
 		   int y4=sc.nextInt();
 
 		 double length2=Math.sqrt((Math.pow(x4-x3,2))+Math.pow(y4-y3,2));
