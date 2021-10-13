ec56cf8ffaa327803fc98ffa6d7bd0f68591f06c
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-13 13:00:38 +0530

    Use ComareTo Method

--- LineComp.java
+++ LineComp.java
@@ -4,35 +4,37 @@ public class LineComp {
 	public static void main(String[] args){
 		  Scanner sc =new Scanner(System.in);
 		  System.out.println("enter x co-ordinate 1st points");
-		   int x1=sc.nextInt();
-		System.out.println("enter x co-ordinate 2nd points");
-		   int x2=sc.nextInt();
-		System.out.println("enter y co-ordinate 1st points");
-		   int y1=sc.nextInt();
-		System.out.println("enter y co-ordinate 2nd points");
-		   int y2=sc.nextInt();
-		 double length=Math.sqrt((Math.pow(x2-x1,2))+Math.pow(y2-y1,2));
-		   System.out.println("length="+length);
-		 System.out.println("enter x co-ordinate 1st points");
-		    int x3=sc.nextInt();
-		System.out.println("enter x co-ordinate 2nd points");
-		   int x4=sc.nextInt();
-		System.out.println("enter y co-ordinate 1st points");
-		   int y3=sc.nextInt();
-		System.out.println("enter y co-ordinate 2nd points");
-		   int y4=sc.nextInt();
+		  int x1=sc.nextInt();
+		  System.out.println("enter x co-ordinate 2nd points");
+		  int x2=sc.nextInt();
+		  System.out.println("enter y co-ordinate 1st points");
+		  int y1=sc.nextInt();
+		  System.out.println("enter y co-ordinate 2nd points");
+		  int y2=sc.nextInt();
+		  double length=Math.sqrt((Math.pow(x2-x1,2))+Math.pow(y2-y1,2));
+		  System.out.println("length="+length);
+		 
+		  System.out.println("enter x co-ordinate 1st points");
+	          int x3=sc.nextInt();
+		  System.out.println("enter x co-ordinate 2nd points");
+		  int x4=sc.nextInt();
+		  System.out.println("enter y co-ordinate 1st points");
+		  int y3=sc.nextInt();
+		  System.out.println("enter y co-ordinate 2nd points");
+		  int y4=sc.nextInt();
 
-		 double length2=Math.sqrt((Math.pow(x4-x3,2))+Math.pow(y4-y3,2));
-		   System.out.println("length="+length2);
-		  if(length>length2){
-		    System.out.println("line 1 is greater");
-		}
-		  else if (length==length2){
-		   System.out.println("line1 and line2 is equal");
-		}
-		 else{
-		System.out.println("line 2 is greater");
-		}
+		  double length2=Math.sqrt((Math.pow(x4-x3,2))+Math.pow(y4-y3,2));
+		  System.out.println("length="+length2);
+		  int c=length.compareTo(length2);
+		  if(c==0) {
+			  System.out.println("Two lines are equal");
+		  }
+		  else if(c>0) {
+			  System.out.println(" line 1 are greater than line 2");
+		  }
+	          else {
+			  System.out.println("Line 2 is greater than line 1");
+		      }
 
 		}
 }
